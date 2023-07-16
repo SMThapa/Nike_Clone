f21203e151b95f2a131731774ad3beb9b5244402
SMThapa <siddharthmthapa@gmail.com>
2023-07-16 23:24:03 +0530

    file

--- src/pages/OrderSuccess.js
+++ src/pages/OrderSuccess.js
@@ -7,18 +7,18 @@ export const OrderSuccess = () => {
   const data = state.data;
 
   return (
-    <div className="sm:my-40 sm:h-full flex flex-col justify-center items-center">
-        <h1 className="sm:text-xl">Order Summery</h1>
+    <div className="sm:my-40 px-5 h-screen flex flex-col justify-center items-center">
+        <h1 className="text-xl">Order Summery</h1>
         <div className="flex flex-col justify-center items-center  border-black rounded-lg sm:p-12">
             <img className='h-12 w-16' src={logo} alt="" />
-            <div className="sm:my-4 flex flex-col items-center">
+            <div className="my-4 flex flex-col items-center">
                 <p>Thankyou for your order!</p>
                 <p>Your Order ID: {data.id}</p>
             </div>
             <p>Your Order is confirmed.</p>
-            <p>Please check your email ({data.userInfo.email}) for the further details.</p>
-            <p className="sm:my-4">Payment id: someRandom_id</p>
-            <button onClick={()=>navigate("/")} className='bg-blue-500 text-white sm:py-3 sm:px-5 rounded sm:my-2'>Continue Shopping</button>
+            <p className='text-center'>Please check your email ({data.userInfo.email}) for the further details.</p>
+            <p className="my-4">Payment id: someRandom_id</p>
+            <button onClick={()=>navigate("/")} className='bg-blue-500 text-white py-3 px-5 rounded sm:my-2'>Continue Shopping</button>
         </div>
     </div>
   )
